aeff22e40272a44ecbccafa8fd50869dd8effc95
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-16 17:25:28 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -2,12 +2,12 @@ package janani;
 import java.util.Scanner;
 public class Hello {
 	public static void main(String []args){
-//int n;
-//Scanner ja=new Scanner(System.in);
-//n=ja.nextInt();
-//for(int i=0;i<n;i++){
+int n;
+Scanner ja=new Scanner(System.in);
+n=ja.nextInt();
+for(int i=0;i<n;i++){
 	System.out.println("hello");
 }
 
 }
-//}
+}
